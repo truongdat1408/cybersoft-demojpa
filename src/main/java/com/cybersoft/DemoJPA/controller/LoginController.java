455c8bda2e155f031b5ce4a59bafb655dce2394d
truongdat-windows <truongdat1408@gmail.com>
2023-02-15 17:12:34 +0700

    feature/TK789

--- src/main/java/com/cybersoft/DemoJPA/controller/LoginController.java
+++ src/main/java/com/cybersoft/DemoJPA/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.cybersoft.DemoJPA.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class LoginController {
     //de trong
+    public void login() {
+
+    }
 }
